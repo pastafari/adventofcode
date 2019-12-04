c87e90860a8b57246de18942a99f0cbe91c77271
Mohit Thatte <mohit.thatte@gmail.com>
2019-12-04 22:08:12 +0100

    Parametrize count-password

--- advent-2019/src/advent_2019/secure_container.clj
+++ advent-2019/src/advent_2019/secure_container.clj
@@ -19,7 +19,7 @@
   (let [adjacent-digits (partition 2 1 digits)]
     (not-any? (fn [[a b]] (> (int a) (int b))) adjacent-digits)))
 
-(defn valid-password?
+(defn valid-password-1?
   "Does the password satisfy all conditions?"
   [n]
   (let [digits (str n)]
@@ -29,22 +29,22 @@
 
 (defn count-valid-passwords
   "Of all candidate passwords, how many are valid"
-  [start end]
+  [start end valid-password-fn]
   (loop [candidate start
          total 0]
     (if (> candidate end)
       total
       (recur
        (inc candidate)
-       (if (valid-password? candidate)
+       (if (valid-password-fn candidate)
          (inc total)
          total)))))
 
 (defn solve-1
   []
-  (count-valid-passwords 138241 674034))
+  (count-valid-passwords 138241 674034 valid-password-1?))
 
-(deftest test-valid-password?
-  (is (valid-password? 111111))
-  (is (not (valid-password? 223450)))
-  (is (not (valid-password? 123789))))
+(deftest test-valid-password-1?
+  (is (valid-password-1? 111111))
+  (is (not (valid-password-1? 223450)))
+  (is (not (valid-password-1? 123789))))

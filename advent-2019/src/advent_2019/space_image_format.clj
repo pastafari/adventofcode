f60d013708b5814fe3cedd4508ccb4c75f608cc3
Mohit Thatte <mohit.thatte@gmail.com>
2019-12-09 18:43:33 +0100

    Day 8 - Part 2

--- advent-2019/src/advent_2019/space_image_format.clj
+++ advent-2019/src/advent_2019/space_image_format.clj
@@ -1,7 +1,26 @@
 (ns advent-2019.space-image-format
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is]])
+  (:import (java.awt.image BufferedImage)))
+
+(def BLACK 0)
+
+(def WHITE 1)
+
+(def TRANSPARENT 2)
+
+(defn get-color
+  "Finds the first non transparent color for the layer."
+  [layer]
+  (first (remove #{TRANSPARENT} layer)))
+
+(defn layers->colors
+  "Transposes layers and gets each pixel color"
+  [layers width]
+  (let [pixels (apply map vector layers)]
+    (map get-color pixels)))
+
 
 (defn encoded-image->layers
   "Converts an encoded image to layers given width and height"
@@ -39,6 +58,11 @@
            {:min-zeros Integer/MAX_VALUE}
            layers)))
 
+(defn image->colors
+  [image width height]
+  (let [layers (encoded-image->layers image width height)]
+    (partition width (layers->colors layers width))))
+
 (defn solve-1
   [file width height]
   (let [image (read-encoded-image file)
@@ -47,6 +71,21 @@
     (* (count-digits layer-with-min-zeros 1)
        (count-digits layer-with-min-zeros 2))))
 
+(defn solve-2
+  [file width height]
+  (image->colors (read-encoded-image file) width height))
+
+(defn pixels->bitmap-image
+  [pixels width height]
+  (let [image (BufferedImage. 25 6 BufferedImage/TYPE_INT_RGB)]
+    (for [x (range 25)
+          y (range 6)
+          :let [pixel (nth (nth solution-2 y) x)]]
+      (.setRGB image
+               x
+               y
+               (rand-int 255)))
+    image))
 
 (deftest test-count-digits
   (is (= 4 (count-digits [0 1 2 3 0 1 2 3 0 0] 0))))
@@ -54,3 +93,11 @@
 (deftest test-encoded-image->layers
   (is (= [[1 2 3 4 5 6] [7 8 9 0 1 2]]
          (encoded-image->layers [1 2 3 4 5 6 7 8 9 0 1 2] 3 2))))
+
+
+(deftest test-get-color
+  (is (= 0 (get-color [0 2 2 1])))
+  (is (= 1 (get-color [2 2 2 1]))))
+
+(deftest test-image-colors
+  (is (= [[0 1] [1 0]] (image->colors [0 2 2 2 1 1 2 2 2 2 1 2 0 0 0 0] 2 2))))

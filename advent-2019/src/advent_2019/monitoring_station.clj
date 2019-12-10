2838353e515785a0f77248210cfaaaa12310daee
Mohit Thatte <mohit.thatte@gmail.com>
2019-12-10 20:19:02 +0100

    Day 10 - Part 1
    - not fast, but works!

---
+++ advent-2019/src/advent_2019/monitoring_station.clj
@@ -0,0 +1,97 @@
+(ns advent-2019.monitoring-station
+  (:require [clojure.test :refer [deftest is run-tests]]
+            [clojure.java.io :as io]))
+
+
+(defn squared-distance
+  "Square of distance between two asteroids"
+  [[x1 y1] [x2 y2]]
+  (+ (* (- x2 x1) (- x2 x1))
+     (* (- y2 y1) (- y2 y1))))
+
+(defn is-between?
+  "Is asteroid c between a and b?
+  Yes iff cross-product (b-a) x (c-a) is 0
+  AND dot-product (b-a).(c-a) > 0
+  AND squared-distance(a,b) < dot-product"
+  [a b c]
+  (let [[x1 y1] a
+        [x2 y2] b
+        [x3 y3] c
+        cross-product (- (* (- y3 y1) (- x2 x1))
+                         (* (- x3 x1) (- y2 y1)))
+        dot-product (+ (* (- x3 x1) (- x2 x1))
+                       (* (- y3 y1) (- y2 y1)))
+        squared-distance-a-b (squared-distance a b)]
+    (and (zero? cross-product)
+         (pos? dot-product)
+         (> squared-distance-a-b dot-product))))
+
+
+(defn get-visible-asteroids
+  "Counts asteroids that are directly visible from p,
+  i.e. no asteroid in between p and the other asteroid"
+  [p asteroids]
+  (let [other-asteroids (disj asteroids p)
+        line-of-sight (keep (fn [other]
+                              (let [remaining (disj other-asteroids other)]
+                                (when (not-any? #(is-between? p other %)
+                                                remaining)
+                                  other)))
+                            other-asteroids)]
+    line-of-sight))
+
+(defn count-visible-asteroids
+  [p asteroids]
+  {:count (count (get-visible-asteroids p asteroids))
+   :asteroid p})
+
+(defn read-asteroids
+  "Reads file and returns asteroids as a set of points in the x-y plane"
+  [file]
+  (let [lines (-> file
+                  io/resource
+                  io/file
+                  io/reader
+                  line-seq)]
+    ;; acrobatics to build a set of asteroids!
+    (into #{} (mapcat identity
+                      (map-indexed (fn [y line]
+                                     (remove nil? (map-indexed
+                                                   (fn [x p] (when (= p \#) [x y]))
+                                                   line)))
+                                   lines)))))
+
+(defn solve-1
+  [file]
+  (let [asteroids (read-asteroids file)
+        visible-count (map (fn [a] (count-visible-asteroids a asteroids))
+                           asteroids)]    
+    (:count (first (sort-by (comp - :count) visible-count)))))
+
+
+(deftest test-is-between?
+  (is (is-between? [1 0] [4 0] [3 0]))
+  (is (not (is-between? [1 0] [3 0] [4 0])))
+  (is (is-between? [1 0] [3 4] [2 2])))
+
+
+(deftest test-count-visible-asteroids
+  (let [asteroids (read-asteroids "monitoring_station/test-input-1")]
+    (is (= 7 (:count (count-visible-asteroids [1 0] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [4 0] asteroids))))
+    (is (= 6 (:count (count-visible-asteroids [0 2] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [1 2] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [2 2] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [3 2] asteroids))))
+    (is (= 5 (:count (count-visible-asteroids [4 2] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [4 3] asteroids))))
+    (is (= 8 (:count (count-visible-asteroids [3 4] asteroids))))
+    (is (= 7 (:count (count-visible-asteroids [4 4] asteroids))))))
+
+(deftest test-solve-1
+  (is (= 8 (solve-1 "monitoring_station/test-input-1")))
+  (is (= 33 (solve-1 "monitoring_station/test-input-2")))
+  (is (= 35 (solve-1 "monitoring_station/test-input-3")))
+  (is (= 41 (solve-1 "monitoring_station/test-input-4")))
+  (is (= 210 (solve-1 "monitoring_station/test-input-5"))))
